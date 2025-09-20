25c7de35894b1dc4042b54166d8fbd53ab7c5175
Daniel Boroujerdi <274674+DBoroujerdi@users.noreply.github.com>
2025-09-20 13:55:45 +0100

    zsh: network utilities

--- .zsh/functions/network.zsh
+++ .zsh/functions/network.zsh
@@ -1,3 +1,16 @@
 localip() {
     ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'
 }
+
+whoport() {
+    lsof -t -i :$1 -sTCP:LISTEN
+}
+
+killport() {
+    local pid=$(whoport $1)
+    if [[ -n $pid ]]; then
+        kill -9 $pid
+    else
+        echo "No process found on port $1"
+    fi
+}
