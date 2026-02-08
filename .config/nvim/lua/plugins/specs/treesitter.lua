c571589320e1f852e42ff0522f40fcb5ae640f12
Daniel Boroujerdi <274674+DBoroujerdi@users.noreply.github.com>
2026-02-08 15:11:33 +0000

    fix: treesitter error

--- .config/nvim/lua/plugins/specs/treesitter.lua
+++ .config/nvim/lua/plugins/specs/treesitter.lua
@@ -5,19 +5,13 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { 
-          'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 
-          'css', 'javascript', 'typescript', 'json', 'yaml', 'python',
-          'go', 'terraform'
-        },
-        -- Autoinstall languages that are not installed
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-        incremental_selection = { enable = true },
-      }
-    end,
+    opts = {
+      ensure_installed = {
+        'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc',
+        'css', 'javascript', 'typescript', 'json', 'yaml', 'python',
+        'go', 'terraform'
+      },
+      auto_install = true,
+    },
   },
 }
