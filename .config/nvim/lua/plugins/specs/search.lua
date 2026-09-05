f9db159a90651dcff97b3aed9c3372d681ffa768
Daniel Boroujerdi <274674+DBoroujerdi@users.noreply.github.com>
2026-09-05 20:53:14 +0100

    nvim: fold/flash

--- .config/nvim/lua/plugins/specs/search.lua
+++ .config/nvim/lua/plugins/specs/search.lua
@@ -28,7 +28,36 @@ return {
       },
     },
     config = function()
+      local function flash(prompt_bufnr)
+        require('flash').jump {
+          pattern = '^',
+          label = { after = { 0, 0 } },
+          search = {
+            mode = 'search',
+            exclude = {
+              function(win)
+                return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= 'TelescopeResults'
+              end,
+            },
+          },
+          action = function(match)
+            local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+            picker:set_selection(match.pos[1] - 1)
+          end,
+        }
+      end
+
       require('telescope').setup {
+        defaults = {
+          mappings = {
+            i = {
+              ['<c-s>'] = flash,
+            },
+            n = {
+              s = flash,
+            },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
